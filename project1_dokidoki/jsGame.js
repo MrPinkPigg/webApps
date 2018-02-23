ce804b49bebd4aaa8814fca4ec64804f39c9fb15
MrPinkPigg <aaronknestaut@gmail.com>
2018-02-23 10:36:54 -0500

    continued work on doki doki acm

--- project1_dokidoki/jsGame.js
+++ project1_dokidoki/jsGame.js
@@ -1,22 +1,8 @@
-var script = document.createElement('script');
-script.src= "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
-script.type = 'text/javascript';
-
-const $ = require('jquery');
-
 function loadGame () {
 	var audio = new Audio("./assets/music.mp3");
 	audio.play();
-
-	$(function(){
-		$("#start").onLoad(function(){
-        	$("#wal").fadeIn(100);
-    	});
-	})
 }
 
-
-function start () {
-	alert("test");
-}
-
+$(function(){
+	$('#fadeIn').fadeIn(4000);
+});
