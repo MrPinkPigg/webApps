a38adaa890ff30cf0dfb1f1816f5fb39710452d4
MrPinkPigg <aaronknestaut@gmail.com>
2018-03-01 14:23:04 -0500

    worked on the php for online quiz

--- quiz.php
+++ quiz.php
@@ -1,15 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <?php
 
-if (isset($_GET['q1'])) {
-	echo "stuff";
-	echo $_GET["q1"];
-	if ($_GET['q1'] == 1) {echo "stuff 1";}
-}
+	
 
-else {echo "TAKE THE QUIZ";}
 ?>
-
 </html>
